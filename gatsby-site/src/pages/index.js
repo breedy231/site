d6dce16e1be524548baaf7db2f65aad7d7a71bb8
breedy231 <breedy231@gmail.com>
2020-12-09 01:43:16 +0000

    fixing directories and adding gitpod config

--- gatsby-site/src/pages/index.js
+++
@@ -1,5 +0,0 @@
-import React from "react"
-
-export default function Home() {
-  return <div>Hello world! This is some edited Gatsby</div>
-}
---
+++ src/pages/index.js
@@ -0,0 +1,20 @@
+import React from "react"
+
+export default function Home() {
+  return <body>
+      
+    <h1>Brendan Reed</h1>
+    <h2>Software engineer in Boston</h2>
+
+    <div>
+        <h3>Contact Me</h3>
+        <p>
+            <a href="mailto:bren.reed@pm.me">bren.reed@pm.me</a>
+            <br></br>
+            <a href="http://www.github.com/breedy231">Github</a>
+            <br></br>
+            <a href="https://www.linkedin.com/in/brendanreed2/">Linkedin</a>
+        </p>
+    </div>
+    </body>
+}
