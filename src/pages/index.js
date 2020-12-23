4a12425395c7ada06496fbf9a4bb0a4a6adeda46
breedy231 <breedy231@gmail.com>
2020-12-23 22:34:28 +0000

    started on adding layout

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,8 @@
 import React from "react"
 import styled from "styled-components"
 
+import Layout from "../components/layouts"
+
 
 
 const Header = styled.h1`
@@ -10,20 +12,22 @@ const Header = styled.h1`
 
 
 export default function Home() {
-  return <body>
-      
-    <Header>Brendan Reed</Header>
-    <h2>Software engineer in Boston</h2>
-
-    <div>
-        <h3>Contact Me</h3>
-        <p>
-            <a href="mailto:bren.reed@pm.me">bren.reed@pm.me</a>
-            <br></br>
-            <a href="http://www.github.com/breedy231">Github</a>
-            <br></br>
-            <a href="https://www.linkedin.com/in/brendanreed2/">Linkedin</a>
-        </p>
-    </div>
-    </body>
+  return (
+      <Layout>
+        <body>
+            <Header>Brendan Reed</Header>
+            <h2>Software engineer in Boston</h2>
+            <div>
+                <h3>Contact Me</h3>
+                <p>
+                    <a href="mailto:bren.reed@pm.me">bren.reed@pm.me</a>
+                    <br></br>
+                    <a href="http://www.github.com/breedy231">Github</a>
+                    <br></br>
+                    <a href="https://www.linkedin.com/in/brendanreed2/">Linkedin</a>
+                </p>
+            </div>
+        </body>
+      </Layout>
+  ) 
 }
