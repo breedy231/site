25065ff3a96a2f5413232cc04fc356fbc3d3e8db
Brendan Reed <brendanreed@Brendans-MacBook-Pro.local>
2021-09-18 16:00:03 -0400

    added Klaviyo blurb

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,8 +7,10 @@ export default function Home() {
   return (
       <Layout>
         <h1>Brendan Reed</h1>
-        <h2>Software Engineer in Boston</h2>
         <div>
+            <h3>About Me</h3>
+            <p>I'm currently employed at Klaviyo, where I work on the Reporting team as a full-stack software engineer. Using Python, React, and Typescript, I enjoy designing and developing new tools to help people understand their data. </p>
+            <p>I also enjoy cycling, films, 3D printing, and cooking.</p>
             <h3>Contact Me</h3>
             <p>
                 <a href="mailto:bren.reed@pm.me">bren.reed@pm.me</a>
