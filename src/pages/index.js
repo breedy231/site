01ce6eeaa176776771d3f52fb0213226846ff328
breedy231 <breedy231@gmail.com>
2021-09-18 16:24:35 -0400

    copy changes

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,16 +9,14 @@ export default function Home() {
         <h1>Brendan Reed</h1>
         <div>
             <h3>About Me</h3>
-            <p>I'm currently employed at Klaviyo, where I work on the Reporting team as a full-stack software engineer. Using Python, React, and Typescript, I enjoy designing and developing new tools to help people understand their data. </p>
+            <p>I'm currently employed at Klaviyo, where I work on the Reporting team as a full-stack software engineer. Using Python, React, and Typescript, I enjoy designing and developing accessible tools to help people understand their data.</p>
             <p>I also enjoy cycling, films, 3D printing, and cooking.</p>
             <h3>Contact Me</h3>
-            <p>
-                <a href="mailto:bren.reed@pm.me">bren.reed@pm.me</a>
-                <br></br>
-                <a href="http://www.github.com/breedy231">Github</a>
-                <br></br>
-                <a href="https://www.linkedin.com/in/brendanreed2/">Linkedin</a>
-            </p>
+            <a href="mailto:bren.reed@pm.me">bren.reed@pm.me</a>
+            <br></br>
+            <a href="http://www.github.com/breedy231">Github</a>
+            <br></br>
+            <a href="https://www.linkedin.com/in/brendanreed2/">Linkedin</a>
         </div>
       </Layout>
   ) 
