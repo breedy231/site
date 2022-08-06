64e9b880ff75d9d2341b77799223bdad78d92fd9
breedy231 <breedy231@gmail.com>
2022-08-06 19:31:44 -0400

    added react-media and started to work on the mobile layout

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,17 +1,18 @@
-import React from "react"
+import React, { Fragment } from "react"
+import Media from 'react-media';
 import styled from "styled-components"
 
 import Layout from "../components/layout"
 
 
-const StyledHeader = styled.p`
-    font-family: 'Poppins Bold' ;
-    font-style: normal;
-    font-weight: 700;
-    font-size: 40px;
-    line-height: 48px;
-    color: #DA300F;
-`
+const StyledHeader = styled.p(props => ({
+    'font-family': 'Poppins Bold',
+    'font-style': 'normal',
+    'font-weight': 700,
+    'font-size': props.small ? '20px' : '40px',
+    'line-height': '48px',
+    color: '#DA300F',
+}));
 
 const StyledContentDiv = styled.div`
     display: flex;
@@ -25,18 +26,19 @@ const StyledContentDiv = styled.div`
     align-items: center;
 `;
 
-const StyledTextDiv = styled.div`
-    max-width: 700px;
-`;
+const StyledTextDiv = styled.div(props => ({
+    'max-width': props.small ? '300px' : '700px',
+    'margin-left': props.small ? '200px' : undefined,
+}));
 
-const StyledMainText = styled.p`
-    font-family: 'Poppins Regular';
-    font-style: normal;
-    font-weight: 700;
-    font-size: 40px;
-    line-height: 48px;
-    color: #FFFFFF;
-`
+const StyledMainText = styled.p(props => ({
+    'font-family': 'Poppins Regular',
+    'font-style': 'normal',
+    'font-weight': 700,
+    'font-size': props.small ? '20px' : '40px',
+    'line-height': props.small ? '24px' : '48px',
+    'color': '#FFFFFF',
+}));
 
 const StyledSubText = styled.p`
     font-family: 'Poppins Regular';
@@ -94,11 +96,24 @@ const MiniRow = styled.div`
 
 export default function New() {
   return (
-      <Layout>
+    <Media queries={{
+        small: "(max-width: 599px)",
+        medium: "(min-width: 600px) and (max-width: 1199px)",
+        large: "(min-width: 1200px)"
+      }}>
+        {matches => (
+            <Fragment>
+              {matches.small && (
+              <p>I am small!</p>
+          )}
+              {matches.medium && <p>I am medium!</p>}
+              {(matches.large || matches.small) && (
+              
+              <Layout>
         <StyledContentDiv>
-            <StyledTextDiv>
-                <StyledHeader>Brendan Reed</StyledHeader>
-                <StyledMainText>
+            <StyledTextDiv small={matches.small}>
+                <StyledHeader small={matches.small}>Brendan Reed</StyledHeader>
+                <StyledMainText small={matches.small}>
                      I'm a software engineer. I create delightful user experiences designed to help people engage with their data. 
                 </StyledMainText>
                 <StyledSubText>
@@ -117,5 +132,10 @@ export default function New() {
             </StyledTextDiv>
         </StyledContentDiv>
       </Layout>
+          )}
+    </Fragment>
+          )}
+     
+    </Media>
   ) 
 }
