a3e2a7f2c0d4a3ac22dcca02f43438468aef7ce6
breedy231 <breedy231@gmail.com>
2023-02-06 21:46:28 -0600

    tring a fix for a11y plugin

--- src/pages/index.js
+++ src/pages/index.js
@@ -113,6 +113,20 @@ const MiniRow = styled.div`
   flex-direction: row;
 `
 
+const SEO = ({ children }) => (
+  <>
+    {children}
+  </>
+)
+
+export const Head = () => (
+  <SEO>
+    <title>Hello World</title>
+    <html lang="en" />
+  </SEO>
+)
+
+
 export default function New() {
   return (
     <Media
