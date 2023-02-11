bfb0c4926975a36b3d384a89bc992dedeca16adb
breedy231 <breedy231@gmail.com>
2023-02-11 15:16:23 -0600

    added parsing

--- src/api/plex.js
+++ src/api/plex.js
@@ -1,7 +1,8 @@
 export default function handler(req, res) {
 
     if (req.method == 'POST') {
-        res.status(200).json({ 'message': `received event type ${req.body['event']}` })
+        const eventType = JSON.parse(req.body.payload)['event'];
+        res.status(200).json({ 'message': `received event type ${eventType}` })
     }
 
     res.status(200).json({ 'message': `hit plex API endpoint` })
