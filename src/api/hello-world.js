26b2e06cefa81dd92a9bd9f3f9b4b5cfc1b630eb
reed.br <reed.br@husky.neu.edu>
2023-02-12 11:52:28 -0600

    removing yarn lock & adding more support for processing webhook after local test

--- src/api/hello-world.js
+++ src/api/hello-world.js
@@ -1,3 +1,3 @@
-export default function handler(req, res) {
-    res.status(200).json({ hello: `world` })
-  }
+export default function API (req, res) {
+  res.json({ hello: "world" })
+}
--- src/api/plex.js
+++ src/api/plex.js
@@ -1,9 +1,44 @@
+
+
+const getImageFromPlex = async (thumbUri) => {
+    const image = await request.get({
+        uri: thumbUri,
+        encoding: null
+    })
+    return image
+}
+
 export default function handler(req, res) {
+    const eventType = JSON.parse(req.body.payload)['event'];
+
+    if (eventType == 'media.play') {
+        // console.log(req);
+
+
+        if (req.files) {
+            console.log(req.files[0].buffer)
+        }
+    }
+
+
 
     if (req.method == 'POST') {
         const eventType = JSON.parse(req.body.payload)['event'];
-        res.status(200).json({ 'message': `received event type ${eventType}` })
-    }
+        // console.log(JSON.parse(req.body.payload))
 
+        if (req.file && req.file.buffer) {
+            console.log('event has image')
+            const buffer = req.file.buffer;
+            console.log(buffer)
+        }
+
+        // if (req['Metadata']['thumb']) {
+        //     const image = getImageFromPlex(req['Metadata']['thumb']);
+        //     console.log(image);
+        // }
+
+
+        console.log(eventType)
+    }
     res.status(200).json({ 'message': `hit plex API endpoint` })
 }
