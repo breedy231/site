1e400d8da21893164cfa237d178d1604e08b4883
breedy231 <breedy231@gmail.com>
2023-02-19 17:00:10 -0600

    cleaned up response

--- src/api/plex.js
+++ src/api/plex.js
@@ -3,9 +3,7 @@ import axios from "axios"
 export default async function handler(req, res) {
   if (req.method == "POST") {
     const eventType = JSON.parse(req.body.payload)["event"]
-
     if (eventType == "media.play") {
-      console.log("in play event")
       if (req.files.length > 0) {
         // Format image thumbnail as a base-64 URLEncoded string
         const thumbnailBuffer = req.files[0].buffer
@@ -28,7 +26,7 @@ export default async function handler(req, res) {
             .then(res => {
               return res
             })
-          res.json(result)
+          res.status(200).json({message: 'triggered site build', data: result.data})
         } catch (error) {
           res.status(500).send(error)
         }
