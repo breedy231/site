42aa83b8fca00f87fb6b69c987f412f14159e2d8
breedy231 <breedy231@gmail.com>
2023-02-19 16:51:04 -0600

    more cleanup

--- src/api/plex.js
+++ src/api/plex.js
@@ -5,31 +5,29 @@ export default async function handler(req, res) {
     const eventType = JSON.parse(req.body.payload)["event"]
 
     if (eventType == "media.play") {
-      console.log('in play event')
+      console.log("in play event")
       if (req.files.length > 0) {
-        // Save thumbnail buffer file & event metadata markdown file
+        // Format image thumbnail as a base-64 URLEncoded string
         const thumbnailBuffer = req.files[0].buffer
-
-        // const encoded = urlEncodeBytes(thumbnailBuffer)
-        console.log(thumbnailBuffer.toString('base64'))
-
         const payload = {
-          thumbnailBufferEncoded: thumbnailBuffer.toString('base64')
+          thumbnailBufferEncoded: thumbnailBuffer.toString("base64"),
         }
 
         // Send POST request to build hook
         try {
-          const result = await axios.post(
-            "https://api.netlify.com/build_hooks/63e946b0744a8f42bee25c24",
-            payload,
-            {
-              headers: {
-                'Content-Type': 'application/x-www-form-urlencoded'
+          const result = await axios
+            .post(
+              "https://api.netlify.com/build_hooks/63e946b0744a8f42bee25c24",
+              payload,
+              {
+                headers: {
+                  "Content-Type": "application/x-www-form-urlencoded",
+                },
               }
-            }
-          ).then((res) => {
-            return res
-          })
+            )
+            .then(res => {
+              return res
+            })
           res.json(result)
         } catch (error) {
           res.status(500).send(error)
