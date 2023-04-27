4d942aeb879441abc367af022b649038f5d877ee
breedy231 <breedy231@gmail.com>
2023-04-27 18:55:28 -0500

    refactored now playing

--- src/pages/now_playing.js
+++ src/pages/now_playing.js
@@ -4,8 +4,6 @@ import Layout from "../components/layout"
 import { GatsbyImage } from "gatsby-plugin-image"
 import { useStaticQuery, graphql } from "gatsby"
 
-import { TextDiv, Header, StyledSubText } from "../components/textElements"
-
 export const Head = () => (
   <>
     <title>Hello World</title>
@@ -36,50 +34,29 @@ export default function NotFound() {
         large: "(min-width: 1200px)",
       }}
     >
-      {matches => (
+      {() => (
         <Fragment>
-          {matches.small && !matches.large && (
-            <Layout>
-              <div className="text-5x flex flex-col items-center justify-center">
-                <TextDiv small={matches.small}>
-                  <Header small={matches.small}>Now Playing</Header>
-                  <StyledSubText small={matches.small}>
-                    The latest from my Plex Server
-                  </StyledSubText>
-                </TextDiv>
-                {data.allFile.edges.length > 0 ? (
-                  <GatsbyImage
-                    image={
-                      data.allFile.edges[0].node.childImageSharp.gatsbyImageData
-                    }
-                  />
-                ) : (
-                  <p>No image to display</p>
-                )}
+          <Layout>
+            <div className="text-5x flex flex-col items-center justify-center">
+              <div className="mt-14 text-3xl text-white md:text-4xl">
+                <p className="text-4xl font-bold text-red-600 md:text-5xl">
+                  Now Playing
+                </p>
+                <p className="mt-10 text-lg text-white md:text-4xl">
+                  The latest from my Plex Server
+                </p>
               </div>
-            </Layout>
-          )}{" "}
-          {matches.large && !matches.small && (
-            <Layout>
-              <div className="text-5x flex flex-col items-center justify-center">
-                <TextDiv small={matches.small}>
-                  <Header small={matches.small}>Now Playing</Header>
-                  <StyledSubText small={matches.small}>
-                    The latest from my Plex Server
-                  </StyledSubText>
-                </TextDiv>
-                {data.allFile.edges.length > 0 ? (
-                  <GatsbyImage
-                    image={
-                      data.allFile.edges[0].node.childImageSharp.gatsbyImageData
-                    }
-                  />
-                ) : (
-                  <p>No image to display</p>
-                )}
-              </div>
-            </Layout>
-          )}
+              {data.allFile.edges.length > 0 ? (
+                <GatsbyImage
+                  image={
+                    data.allFile.edges[0].node.childImageSharp.gatsbyImageData
+                  }
+                />
+              ) : (
+                <p>No image to display</p>
+              )}
+            </div>
+          </Layout>
         </Fragment>
       )}
     </Media>
