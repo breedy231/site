5cc359a138d8b1f784b3136e0cf1e991b841e7ff
breedy231 <breedy231@users.noreply.github.com>
2025-01-27 05:23:33 +0000

    Prettified Code!

--- netlify/functions/lastfm.js
+++ netlify/functions/lastfm.js
@@ -10,10 +10,10 @@ export default async function handler(req, res) {
   try {
     const [recentTracksRes, topTracksRes] = await Promise.all([
       fetch(
-        `https://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=${USERNAME}&api_key=${API_KEY}&format=json&limit=5`
+        `https://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=${USERNAME}&api_key=${API_KEY}&format=json&limit=5`,
       ),
       fetch(
-        `https://ws.audioscrobbler.com/2.0/?method=user.gettoptracks&user=${USERNAME}&api_key=${API_KEY}&format=json&limit=3&period=7day`
+        `https://ws.audioscrobbler.com/2.0/?method=user.gettoptracks&user=${USERNAME}&api_key=${API_KEY}&format=json&limit=3&period=7day`,
       ),
     ])
 
